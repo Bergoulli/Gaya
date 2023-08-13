428968a1b87015fb0ff0726f5876904f8a2c7770
Bergoulli <mateuscoslop2@gmail.com>
2023-08-13 20:12:53 -0300

    Update model.py

--- nlu/model.py
+++ nlu/model.py
@@ -1,7 +1,10 @@
-'''
+
 import yaml
 import numpy as np
 import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Embedding
+from keras.utils import to_categorical
 
 
 
@@ -40,103 +43,36 @@ print('Maior seq:', max_seq)
 #criar datasex disperso (número de exemplos, tamanho da sequência_
 input_data = np.zeros((len(inputs), max_seq, len(chars)), dtype='int32')
 
-for i, input in enumerate(inputs):
-    for k, ch in enumerate(input):
-        input_data[i, k, chr2idx[ch]] = 1.0
-
-print(input_data[0])
-
-
-print(inputs)
-print(outputs)
-
-
-'''
-import yaml
-import numpy as np
-
-data = yaml.safe_load(open('nlu\\train.yml', 'r', encoding='utf-8').read())
-
-inputs, outputs = [], []
-
-for command in data['commands']:
-    inputs.append(command['input'].lower())
-    outputs.append('{}\{}'.format(command['entity'], command['action']))
-
-
-# Processar texto: palavras, caracteres, bytes, sub-palavras
-
-chars = set()
-
-for input in inputs + outputs:
-    for ch in input:
-        if ch not in chars:
-            chars.add(ch)
-
-# Mapear char-idx
-
-chr2idx = {}
-idx2chr = {}
-
-for i, ch in enumerate(chars):
-    chr2idx[ch] = i
-    idx2chr[i] = ch
-
-
-max_seq = max([len(x) for x in inputs])
-
-print('Número de chars:', len(chars))
-print('Maior seq:', max_seq)
-
-# Criar dataset one-hot (número de examplos, tamanho da seq, num caracteres)
-# Criar dataset disperso (número de examplos, tamanho da seq)
-
-# Input Data one-hot encoding
-
-input_data = np.zeros((len(inputs), max_seq, len(chars)), dtype='int32')
-for i, input in enumerate(inputs):
-    for k, ch in enumerate(input):
-        input_data[i, k, chr2idx[ch]] = 1.0
-
-
-
-# Input data sparse
-
-input_data = np.zeros((len(inputs), max_seq), dtype='int32')
-
-for i, input in enumerate(inputs):
-    for k, ch in enumerate(input):
-        input_data[i, k] = chr2idx[ch]
-
-# Output Data
+#Criar labels para o classificador
 
 labels = set(outputs)
-
-label2idx = {}
-idx2label = {}
+labels2idx = {}
+idx2lbel = {}
 
 for k, label in enumerate(labels):
-    label2idx[label] = k
-    idx2label[k] = label
+    labels2idx[label] = k
+    idx2lbel[k] = label
 
 output_data = []
 
 for output in outputs:
-    output_data.append(label2idx[output])
+    output_data.append(labels2idx[output])
 
 output_data = to_categorical(output_data, len(output_data))
 
+for i, input in enumerate(inputs):
+    for k, ch in enumerate(input):
+        input_data[i, k, chr2idx[ch]] = 1.0
 
 print(output_data[0])
 
 model = Sequential()
-model.add(Embedding(len(chars), 64))
-model.add(LSTM(128, return_sequences=True))
+model.add(Embedding(len(chars), 128))
+model.add(LSTM(128))
 model.add(Dense(len(output_data), activation='softmax'))
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
-
-model.fit(input_data, output_data, epochs=16)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc']) 
+model.summary()
 
 
 '''
