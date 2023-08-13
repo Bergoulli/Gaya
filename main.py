31862df1fd677c41929d3cb886a95b6b98cb9dc9
Bergoulli <mateuscoslop2@gmail.com>
2023-08-13 12:22:42 -0300

    adionar modelo vosk

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-# para inicial do arquivo com menu
+from vosk import Model, KaldiRecognizer
+import os
+import pyaudio
 
-import speech_recognition as sr
+model = Model('model')
+rec = KaldiRecognizer(model, 16000)
 
-#criando uma porta de reconhecimento
-r = sr.Recognizer()
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
+
+while True:
+    data = stream.read(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
+
+print(rec.FinalResult())
